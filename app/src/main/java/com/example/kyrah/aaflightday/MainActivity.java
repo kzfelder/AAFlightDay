b6af3c6798ede386245bd559ff9b04302040e44d
kzfelder <35270718+kzfelder@users.noreply.github.com>
2018-10-07 02:01:15 -0700

    change get request to use json objects

--- app/src/main/java/com/example/kyrah/aaflightday/MainActivity.java
+++ app/src/main/java/com/example/kyrah/aaflightday/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.kyrah.aaflightday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,28 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void getFlightData(){
         final TextView mTextView = (TextView) findViewById(R.id.testTV);
-        // ...
-
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
         String url = "https://xuwcd.herokuapp.com/reservation?recordLocator=NIGRWW";
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
                     @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response);
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.e("KSWAGTHAGOAT", "onResponse: " + response.get("_id"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        mTextView.setText("Response: " + response);
                     }
                 }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work!");
-            }
-        });
 
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
 }
